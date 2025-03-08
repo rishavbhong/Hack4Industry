51b6b9b4e41a639eda94441fd3a77f605615276c
Rishav Kumar <94066370+rishavbhong@users.noreply.github.com>
2025-03-08 19:08:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const sectionId = this.getAttribute('href').substring(1);
+        document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector("nav ul");
+
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
+
